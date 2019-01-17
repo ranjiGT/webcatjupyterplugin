d9b0a57c79ef11164a84e426bab1e56652e9d8ec
hamzamanzoor <hamzamanzoor@hotmail.com>
2019-01-17 14:01:16 -0500

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+import setuptools
+
+setuptools.setup(
+    name="webcatjupyterplugin",
+    version='0.1.0',
+    url="https://github.com/CSSPLICE/webcatjupyterplugin",
+    author="Hamza Manzoor",
+    description="Jupyter Notebook Plugin for submitting files to Web-CAT",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'notebook'
+    ],
+    package_data={'webcatjupyterplugin': ['static/*']},
+)
