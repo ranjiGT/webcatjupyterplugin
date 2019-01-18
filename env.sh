bcccfbbb47d03786173427367a40557dc6bbd75b
hamzamanzoor <hamzamanzoor@hotmail.com>
2019-01-18 00:34:43 -0500

    Add env.sh

---
+++ env.sh
@@ -0,0 +1,6 @@
+####################### Changes in jupyter config ###########################
+if [ ! -f ~/.jupyter/jupyter_notebook_config.py ]; then
+   jupyter notebook --generate-config
+fi
+
+echo 'c.NotebookApp.disable_check_xsrf = True' >> ~/.jupyter/jupyter_notebook_config.py
