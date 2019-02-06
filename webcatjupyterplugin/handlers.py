504737cc9518002417c70a72116a50935f4ce7b2
hamzamanzoor <hamzamanzoor@hotmail.com>
2019-02-06 13:46:22 -0500

    Fix lint issues

--- webcatjupyterplugin/handlers.py
+++ webcatjupyterplugin/handlers.py
@@ -1,7 +1,15 @@
+"""This file receives a request from the front-end Jupyter extension and pushes 
+the file to Web-CAT"""
+
+import os
+import json
+import urllib
+import requests
+
 from notebook.utils import url_path_join as ujoin
 from notebook.base.handlers import IPythonHandler
-import os, json, git, urllib, requests
 from bs4 import BeautifulSoup
+
 
 class WebCatPushHandler(IPythonHandler):
 
@@ -31,28 +39,30 @@ class WebCatPushHandler(IPythonHandler):
             'a': assignment,
             'd': institute
             };
+
         url = 'https://web-cat.cs.vt.edu/Web-CAT/WebObjects/Web-CAT.woa/wa/submit'
 
         filepath = cwd+"/"+filename
 
         files = {'file1': open(filepath, 'rb')}
-        redirectLink = ""
+        redirect_link = ""
         try:
             response = requests.post(url, data=payload, files=files)
             soup = BeautifulSoup(response.content, "html.parser")
             for link in soup.findAll('a'):
-                redirectLink=link.get('href')
+                redirect_link = link.get('href')
         except Exception as e:
-            print("Error")
             print(e)
-            self.error_and_return(cwd, "Could send request to",e)
+            self.error_and_return(cwd, "Could send request because: "+ str(e))
             return
 
         # return to directory
         os.chdir(cwd)
 
         # close connection
-        self.write(json.dumps({'status': 200, 'statusText': cwd+"/"+filename, 'responseText': response.content.decode("utf-8"), 'redirectLink': redirectLink}))
+        self.write(json.dumps({'status': 200, 'statusText': cwd+"/"+filename, 
+            'responseText': response.content.decode("utf-8"), 
+            'redirectLink': redirect_link}))
 
 
 def setup_handlers(nbapp):
