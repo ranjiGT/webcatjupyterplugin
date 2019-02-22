ac4be9e3af0fd9612918a4aaa3caae005198641d
hamzamanzoor <hamzamanzoor@hotmail.com>
2019-02-22 11:25:46 -0500

    auto-save notebooks

--- webcatjupyterplugin/static/main.js
+++ webcatjupyterplugin/static/main.js
@@ -34,7 +34,7 @@ define([
         function webcat_request() {
             var re = /^\/notebooks(.*?)$/;
             var filepath = window.location.pathname.match(re)[1];
-            Jupyter.menubar.notebook.save_notebook();
+            Jupyter.actions.call("jupyter-notebook:save-notebook");
             try {
                 var cell = Jupyter.notebook.get_cell(0);
                 var text = cell.get_text();
