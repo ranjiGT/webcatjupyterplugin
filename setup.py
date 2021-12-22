42706188db23ee7ac32bf58e742ef8344c7e22ec
Ranji Raj <64376506+ranjiGT@users.noreply.github.com>
2021-12-22 16:20:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ import setuptools
 setuptools.setup(
     name="webcatjupyterplugin",
     version='0.1.0',
-    url="https://github.com/CSSPLICE/webcatjupyterplugin",
-    author="Hamza Manzoor",
+    url="https://github.com/ranjiGT/webcatjupyterplugin",
+    author="Ranji Raj",
     description="Jupyter Notebook Plugin for submitting files to Web-CAT",
     packages=setuptools.find_packages(),
     install_requires=[
