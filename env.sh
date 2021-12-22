6bdf55e7deb59f18798a9f1d11410b32915f49fa
Ranji Raj <64376506+ranjiGT@users.noreply.github.com>
2021-12-22 16:15:08 +0100

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 ####################### Changes in jupyter config ###########################
-if [ ! -f ~/.jupyter/jupyter_notebook_config.py ]; then
-   jupyter notebook --generate-config
+if [ ! -f ~/.jupyter/jupyterhub_config.py ]; then
+   jupyterhub --generate-config
 fi
 
-echo 'c.NotebookApp.disable_check_xsrf = True' >> ~/.jupyter/jupyter_notebook_config.py
+echo 'c.NotebookApp.disable_check_xsrf = True' >> ~/.jupyter/jupyterhub_config.py
